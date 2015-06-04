747ed07105baa4ed0e2d2c51cc83f3a9aef68e4c
supatsorn <minnydeluffy@gmail.com>
2015-06-04 16:29:50 -0700

    แก้ไขไฟล์
    แก้ไขไฟล์ login1
    แก้ไขไฟล์ login2
    เพิ่มไฟล์ index

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>
+mint sever
+</title>
+</head>
+<body>
+<center><h1>หน้าหลัก</h1></center>
+<form action="" method="post">
+<input type="submit" value="Logout" />
+</form>
+<form action="" method="">
+<table border="1" align="center" width="100%" height="90%">
+<tr colspan="2" align="center"><td height="65"><h3>ข้อความจากเพื่อน</h3></td><td><h3>ส่งขอ้ความถึงเพื่อน</h3></td>
+</tr>
+<tr><td>
+จาก  : <br /><textarea name="mass1" cols="" rows=""></textarea>
+</td>
+<td rowspan="3" align="center">ส่งข้อควมถึง : <input name="text1" type="text" /><br /> <textarea name="mass4" cols="" rows=""></textarea>
+<input type="submit" value="ส่งข้อความ" /></td><br />
+</tr>
+<tr><td>
+จาก  : <br /><textarea name="mass2" cols="" rows=""></textarea>
+</td></tr>
+<tr><td>
+จาก  : <br /><textarea name="mass3" cols="" rows=""></textarea>
+</td></tr>
+</table>
+</form>
+</body>
+</html>
